886b0c4d4fa0662c54a86020ba946e14038158c6
zohir kioukiou <kiouzohir@gmail.com>
2023-08-06 20:42:54 +0100

    completed assginments

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solutions in this file
+// Code your solutions
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const gifters =[ "Guadalupe", "Ollie" , "Aki"]
+function writeCards(gifters , surprise) {
+  const message =[]
+  for (let i = 0; i < gifters.length; i++) {
+    message.push(`Thank you, ${gifters[i]}, for the wonderful ${surprise} gift!`);;
+  }
+  return message
+}
+writeCards(gifters, 'surprise')
+
+// function countDown(numbers){
+//   console.log(numbers);
+//   let countDown = 10;
+//   while (countDown < 10) {
+//     console.log(`Hello its me ${numbers}` );countDown++
+// }
+// return numbers
+// }
+function countDown() {
+  let countup = 10;
+  while (countup >= 0) {
+    console.log(countup--);
+  }
+}
+countDown()
